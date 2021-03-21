075d49dcdf74329d3aefe62ee11e17b4a8fc5d2f
Justin Mello <justinmello32@hotmail.com>
2021-03-21 20:45:11 +0000

    Adding test cases and additional logic.

--- hw3_2.cpp
+++ hw3_2.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
-int main() {
 
-    cout << "Hello, World!" << endl;
-    return 0;
+
+int main()
+{
+    //Gather number of vertices
+    int vertices;
+    int edges;
+    cin >> vertices;
+    
+    //vector<vector<int>> graph( vertices , vector<int> (vertices, INT_MAX));
+    
+    //Gather the Cities
+    vector<string> cities(vertices);
+    for(int i = 0; i < vertices; i++) {
+        cin >> cities[i];
+    }
+    
+    cin >> edges;
+    
+    string loc1, loc2;
+    int intLoc1 =0;
+    int intLoc2 = 0;
+    
+    int cost;
+    
+    for(int i = 0; i < edges ; i++) {
+        
+        cin >> loc1; 
+        
+        for(int i = 0; i < cities.size(); i++) {
+            if(loc1 == cities[i])
+                intLoc1 = i;
+        }
+        
+        cin >> loc2;
+        for(int i = 0; i < cities.size(); i++) {
+            if(loc2 == cities[i])
+                intLoc2 = i;
+        }
+        
+        cin >> cost;
+        
+        
+        
+    }
+        cout << "Location 1 is " << intLoc1 << endl;
+        cout << "Location 2 is " << intLoc2 << endl;
+       
+        
+        return 0;
 }
+
